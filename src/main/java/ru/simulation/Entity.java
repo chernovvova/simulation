77e4f01f8515e56d8526bc0091479a4cce3ac6f2
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-07-22 14:24:43 +0300

    fix: add missing argument to setters

--- src/main/java/ru/simulation/Entity.java
+++ src/main/java/ru/simulation/Entity.java
@@ -17,11 +17,11 @@ public abstract class Entity {
         return y;
     }
 
-    public void setX() {
+    public void setX(int x) {
         this.x = x;
     }
 
-    public void setY() {
+    public void setY(int y) {
         this.y = y;
     }
 
