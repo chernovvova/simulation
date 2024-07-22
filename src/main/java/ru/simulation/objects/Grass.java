36b4e02b22b1a1f510f9876755b431d9b60818d5
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-07-22 12:42:18 +0300

    Add classes for static objects in simulation

--- src/main/java/ru/simulation/objects/Grass.java
+++ src/main/java/ru/simulation/objects/Grass.java
@@ -1,5 +1,9 @@
 package main.java.ru.simulation.objects;
 
-public class Grass {
+import main.java.ru.simulation.Entity;
 
+public class Grass extends Entity{
+    public Grass(final int x, final int y) {
+        super(x, y);
+    }
 }
--- src/main/java/ru/simulation/objects/Rock.java
+++ src/main/java/ru/simulation/objects/Rock.java
@@ -1,6 +1,6 @@
-package main.objects;
+package main.java.ru.simulation.objects;
 
-import main.Entity;
+import main.java.ru.simulation.Entity;
 
 public class Rock extends Entity{
     public Rock(final int x, final int y) {
