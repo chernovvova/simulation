6233aaa88c054df6f1a0a900727c348f5fd66f50
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-07-22 12:01:38 +0300

    Add creature class

---
+++ src/Creature.java
@@ -0,0 +1,28 @@
+public abstract class Creature extends Entity {
+    private int health;
+    private int speed;
+
+    public Creature(int x, int y, int health, int speed) {
+        super(x, y);
+        this.health = health;
+        this.speed = speed;
+    }
+
+    public abstract void makeMove();
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
