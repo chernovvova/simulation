bdaa5f5ff7f3f589f70e182a66aa60e94f32c061
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-07-22 18:07:36 +0300

    feat: add constructors

--- src/main/java/ru/simulation/creatures/Herbivore.java
+++ src/main/java/ru/simulation/creatures/Herbivore.java
@@ -1,5 +1,9 @@
 package main.java.ru.simulation.creatures;
 
 public class Herbivore extends Creature{
+    public Herbivore(int x, int y, int health, int speed) {
+        super(x, y, health, speed);
+        this.setEntityType("Herbivore");
+    }
 
 }
--- src/main/java/ru/simulation/creatures/Predator.java
+++ src/main/java/ru/simulation/creatures/Predator.java
@@ -1,5 +1,10 @@
 package main.java.ru.simulation.creatures;
 
 public class Predator extends Creature {
+    private int damage;
 
+    public Predator(int x, int y, int health, int speed, int damage) {
+        super(x, y, health, speed);
+        this.damage = damage;
+    }
 }
